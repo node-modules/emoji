ecbb78e8b71d3cb5fe1b686ac0aa003337fe65fa
fengmk2 <fengmk2@gmail.com>
2013-05-29 14:26:12 +0800

    update missing emoji

--- bin/syncdata.sh
+++ bin/syncdata.sh
@@ -2,6 +2,7 @@
 
 curl -o lib/emoji.png https://raw.github.com/iamcal/php-emoji/master/emoji.png
 curl -o lib/emoji.css https://raw.github.com/iamcal/php-emoji/master/emoji.css
+cat tpl/emoji_missing.css >> lib/emoji.css
 curl -o lib/table.htm https://raw.github.com/iamcal/php-emoji/master/table.htm
 
 python bin/create_emoji_js.py
